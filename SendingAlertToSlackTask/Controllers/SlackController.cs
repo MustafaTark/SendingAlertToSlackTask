0f6fcca549deda20cdcf39fc1c54f35dd002c173
mostafatarek <mostaf01230@gmail.com>
2023-03-02 20:26:36 +0200

    Add project files.

---
+++ SendingAlertToSlackTask/Controllers/SlackController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace SendingAlertToSlackTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SlackController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> ReceiveSpamNotification([FromBody] SpamNotificationPayload payload)
+        {
+            if (payload.Type!.ToLower().Contains("spam"))
+            {
+                // Send Slack alert
+                await SendSlackAlert(payload);
+
+                return Ok();
+            }
+
+            return BadRequest("Not a spam notification");
+        }
+        private async Task SendSlackAlert(SpamNotificationPayload spam)
+        {
+            var slackWebhookUrl = "https://hooks.slack.com/services/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";
+            var httpClient = new HttpClient();
+
+            var payload = new
+            {
+                RecordType = spam.RecordType,
+                Type = spam.Type,
+                TypeCode = spam.TypeCode,
+                Name = spam.Name,
+                Tag = spam.Tag,
+                MessageStream = spam.MessageStream,
+                Description = spam.Description,
+                Email = spam.Email,
+                from = spam.From,
+                BouncedAt = spam.BouncedAt
+           
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(payload.MessageStream), Encoding.UTF8, "application/json");
+            await httpClient.PostAsync(slackWebhookUrl, content);
+
+            
+        }
+    }
+}
---
+++ SendingAlertToSlackTask/SpamNotificationPayload.cs
@@ -0,0 +1,17 @@
+﻿namespace SendingAlertToSlackTask
+{
+    public class SpamNotificationPayload
+    {
+       public string? RecordType { get; set; }
+        public string? Type { get; set; }
+        public int? TypeCode { get; set; }
+        public string? Name { get; set; }
+        public string? Tag { get; set; }
+        public string? MessageStream { get; set; }
+        public string? Description { get; set; }
+        public string? Email { get; set; }
+        public string? From { get; set; }
+        public DateTime BouncedAt { get; set; }
+
+    }
+}
